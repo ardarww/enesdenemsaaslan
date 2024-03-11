eda3dc663c280b25dc65fc6ed055df54a9ec78a7
ardarww <79516457+ardarww@users.noreply.github.com>
2024-03-11 17:32:49 +0300

    hghtghgf

---
+++ pages/partners.jsx
@@ -0,0 +1,55 @@
+
+export default function Partners() {
+
+    return (
+        <>
+            <div className="w-full my-10">
+            <div className="flex w-full justify-center">
+      <img width="400" src="/img/bck4.png" />
+      </div>
+                <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
+                    <i className="fal fa-stars text-amber-400 mr-2" />
+                    Partners
+                </h1>
+            </div>
+
+            <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+
+            <div className="flex flex-col justify-center text-white rounded">
+            <a href="discord.com">
+                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/avatars/1216390143775211530/faaf62d04e326108b6779667fa939e12.png" />
+                            <div>
+                                <h1 className="leading-none text-3xl font-bold text-white">Partner aranıyor</h1>        
+                                <div className="flex items-center mt-1">
+                                    <p className="font-normal font-sm">
+                                      partner aranıyor..
+                                    </p>
+                                </div>     
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+
+
+                            <div className="flex flex-col justify-center text-white rounded">
+            <a href="discord.com">
+                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/avatars/1216390143775211530/faaf62d04e326108b6779667fa939e12.png" />
+                            <div>
+                                <h1 className="leading-none text-3xl font-bold text-white">Partner Aranıyor..</h1>        
+                                <div className="flex items-center mt-1">
+                                    <p className="font-normal font-sm">
+                                  Partner Aranıyor....
+                                    </p>
+                                </div>     
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+
+                </div>
+                <div className="py-10"></div>
+        </>
+    );
+};
